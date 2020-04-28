7af528826d4e47340f70faecfea9adfdbb2bdf2b
Ya (Quinn) Huang <quinnstumbles@gmail.com>
2020-04-28 16:13:29 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -9,10 +9,6 @@ node = request.XenVM("node")
 node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
 node.routable_control_ip = "true"
 
-node.addService(rspec.Execute(shell="/bin/sh",
-                              command="sudo apt update"))
-node.addService(rspec.Execute(shell="/bin/sh",
-                              command='sudo apt-get install openjdk-8-jdk-headless'))
 
 # Print the RSpec to the enclosing page.
 portal.context.printRequestRSpec()
