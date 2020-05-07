305617b164fd93cd1613d639be9574240175ee5f
Ya (Quinn) Huang <quinnstumbles@gmail.com>
2020-05-07 19:14:44 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ echo "This script will automatically install minecraft pre-req"
 apt update & apt upgrade -y 
 apt install -y openjdk-jdk-headless screen
 
-mkdir minecraft
-cd minecraft
+mkdir ~/minecraft
+cd ~/minecraft
 
 wget https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar
 
