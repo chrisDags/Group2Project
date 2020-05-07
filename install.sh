7c7baedf164305e28dd3c7ace20f52d376aa0b43
Ya (Quinn) Huang <quinnstumbles@gmail.com>
2020-05-07 19:38:22 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,6 +10,8 @@ cd ~/minecraft
 
 wget https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar
 
+screen -S "minecraft-server"
+
 java -Xmx1024M -Xms1024M -jar server.jar nogui
 echo "eula=true" > ~/minecraft/eula.txt
 java -Xmx1024M -Xms1024M -jar server.jar nogui
