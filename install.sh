e450de0e9415f5b747350ff8526bddd77048b631
Quinn Huang <quinnstumbles@gmail.com>
2020-05-07 19:03:42 -0400

    first version of script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "This script will automatically install minecraft pre-req"
+
+apt update & apt upgrade -y 
+apt install -y openjdk-jdk-headless screen
+
+mkdir minecraft
+cd minecraft
+
+wget https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar
+
+java -Xmx1024M -Xms1024M -jar server.jar nogui
+echo "eula=true" > ~/eula.txt
+
