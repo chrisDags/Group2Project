bf40048bbb834d5aa36e0a6cbf4bbaf52047eb3c
Ya (Quinn) Huang <quinnstumbles@gmail.com>
2020-05-08 12:07:11 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,16 +3,15 @@
 echo "This script will automatically install minecraft pre-req"
 
 sudo apt update & sudo apt upgrade -y 
+sudo apt install -y openjdk-8-jre-headless screen
+screen -S "minecraft-server"
 
 mkdir ~/minecraft
 cd ~/minecraft
 
 wget https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar
-sudo apt install -y openjdk-8-jre-headless screen
+
 
 java -Xmx1024M -Xms1024M -jar server.jar nogui
 echo "eula=true" > ~/minecraft/eula.txt
-
-
-screen -D -RR
 java -Xmx1024M -Xms1024M -jar server.jar nogui
